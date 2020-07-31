e4ab4f44bc15e777e765e42f32c3f27a45902d0c
katesfadeeva <katesfadeeva@gmail.com>
2020-07-31 17:24:56 +0300

    add: style for app

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,12 @@
 import React from "react";
+import "./search-panel.css";
 
 const SearchPanel = () => {
     const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    }
+
     return <input
-        style={searchStyle}
+        type="text"
+        className="form-control search-input"
         placeholder={searchText}
     />;
 }
