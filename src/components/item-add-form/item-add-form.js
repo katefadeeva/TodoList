c373323f8f7b14025b646d95c39f2ad8c1896e2c
katesfadeeva <katesfadeeva@gmail.com>
2020-08-05 21:56:52 +0300

    add: form and filter

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,16 +3,40 @@ import React, {Component} from "react";
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-  render() {
-    const { onItemAdded } = this.props;
 
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
+  render() {
     return (
-        <div className="item-add-form">
+        <form className="item-add-form d-flex"
+              onSubmit={this.onSubmit}>
+          <input type="text"
+                 className="form-control"
+                 onChange={ this.onLabelChange }
+                 placeholder="What needs to be done"
+                 value={this.state.label}
+          />
           <button type="button"
-                  className="btn btn-info"
-                  onClick={ () => onItemAdded('Hello world') }> Add Item
+                  className="btn btn-info">
+            Add Item
           </button>
-        </div>
+        </form>
     );
   }
 }
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import "./search-panel.css";
 
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
+export default class SearchPanel extends Component {
+    state = {
+        term: ''
+    }
 
-    return <input
-        type="text"
-        className="form-control search-input"
-        placeholder={searchText}
-    />;
+    searchText = 'Type here to search';
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onSearchChange( term );
+    };
+
+    render() {
+        return <input
+            type="text"
+            className="form-control search-input"
+            placeholder={this.searchText}
+            value={this.state.term}
+            onChange={this.onSearchChange}
+        />;
+    }
 }
-
-export default SearchPanel;
